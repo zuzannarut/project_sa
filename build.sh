fa36a5e8caf95a0a05e178d015089c78398ec337
Sambit Sahoo <sambits002@gmail.com>
2024-05-16 05:21:45 -0400

    added minimizer sketch to build.sh file

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@
 # g++ -O2 -std=c++17 -o sa_radix_sort sa_radix_sort.cpp
 # g++ -O2 -std=c++17 -o sa_divsufsort sa_divsufsort.cpp
 g++ -O2 -std=c++17 -o sa_radix sa_radix.cpp
-
+g++ -O2 -std=c++17 -o sketch_minimizer sketch_minimizer.cpp
+# ./sketch_minimizer to run sketch_minimizer
 # g++ -o bfilt xxhash.o bfilt.o
